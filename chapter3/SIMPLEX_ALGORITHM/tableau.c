8f15d6919046a8b65e8c2212a0c764d474db3422
terapoon <kogupi93@gmail.com>
2019-08-26 02:57:59 +0900

    MOD: update tableau.c and tableau.h

--- chapter3/SIMPLEX_ALGORITHM/tableau.c
+++ chapter3/SIMPLEX_ALGORITHM/tableau.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void sweap_tableau(double **tableau, int *swaped_row, int *n_columns, double *scale) {
+
+int search_i(double *tableau, int n_rows, int n_columns) {
+    /* implement here */
+
+    return 0;
+}
+
+int search_j(double *tableau, int n_rows, int n_columns) {
+    /* implement here */
+
+    return 0;
+}
+
+void devide_row(double **tableau, int devided_row_idx, int devisor_column_idx) {
+    /* implement here */
+
+}
+
+void sweap_tableau(double **tableau, int swaped_row_idx, int n_columns, double sweap_scale) {
     int j = 0;
     for (j=0; j<n_columns; j++) {
+    /* implement here */
+
+    }
+}
--- chapter3/SIMPLEX_ALGORITHM/tableau.h
+++ chapter3/SIMPLEX_ALGORITHM/tableau.h
@@ -1,6 +1,14 @@
 #ifdef __TABLEAU_H_
 #define __TABLEAU_H_
 
-/* implement here */
+
+int search_i(double *tableau, int n_rows, int n_columns);
+
+int search_j(double *tableau, int n_rows, int n_columns);
+
+void devide_row(double **tableau, int devided_row_idx, int devisor_column_idx);
+
+void sweap_tableau(double **tableau, int swaped_row_idx, int n_columns, double sweap_scale);
+
 
 #endif
