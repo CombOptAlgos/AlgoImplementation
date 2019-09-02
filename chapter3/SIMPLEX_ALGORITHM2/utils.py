ec2cd14ca7b92b983137a1300d215881e4c56f38
hirokinishimoto <shozemi.nishimoto@gmail.com>
2019-09-02 23:26:26 +0900

    Mod: LPが非有界のときの処理を追加

--- chapter3/SIMPLEX_ALGORITHM2/utils.py
+++ chapter3/SIMPLEX_ALGORITHM2/utils.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 eps = 1e-5
 
-def make_tableu(A, b):
-    """make initial tableu table
+def make_tableau(A, b):
+    """make initial tableau table
 
     Parameters
     ----------
@@ -29,78 +29,75 @@ def make_tableu(A, b):
     upper = np.c_[A_, np.zeros((m_, m__)), np.diag([1]*m_), np.zeros((m_, m__)), b_]
     middle = np.c_[-A__, np.diag([-1]*m__), np.zeros((m__, m_)), np.diag([1]*m__), -b__]
     lower = np.c_[np.ones((1,m__))@A__, np.ones((1, m__)), np.zeros((1, m+1))]
-    tableu = np.r_[
+    tableau = np.r_[
         upper,
         middle,
         lower
         ]
 
-    return tableu
+    return tableau
 
 
-def get_indices(tableu):
-    """get i, j from tableu
+def get_indices(tableau):
+    """get i, j from tableau
 
     >>> import numpy as np
-    >>> tableu = np.array([
+    >>> tableau = np.array([
         [1, 2, 3],
         [4, 5, 6],
         [-1, 7, 8]
         ])
-    >>> get_indices(tableu)
+    >>> get_indices(tableau)
     1, 0
     """
 
-    n_rows, n_cols = tableu.shape
+    n_rows, n_cols = tableau.shape
 
     i, j = (None, None)
 
     for i_ in range(n_cols-1):
-        if tableu[-1][i_] < -eps:
+        if tableau[-1][i_] < -eps:
             i = i_
             break
     if i is None:
         return i, j # ともにNone
     
     for j_ in range(n_rows-1):
-        if tableu[j_][i] > eps:
+        if tableau[j_][i] > eps:
             if j is None:
                 j = j_
             else:
-                lambda_j_ = culc_lambda(tableu, i, j_)
-                lambda_j = culc_lambda(tableu, i, j)
+                lambda_j_ = culc_lambda(tableau, i, j_)
+                lambda_j = culc_lambda(tableau, i, j)
                 if lambda_j_ < lambda_j:
                     j = j_
-    print(i,j)
-    assert i is not None, "i is None"
-    assert j is not None, "j is None"
     return i, j
 
 
-def culc_lambda(tableu, i, j):
-    return tableu[j][-1] / tableu[j][i]
+def culc_lambda(tableau, i, j):
+    return tableau[j][-1] / tableau[j][i]
 
 
-def sweep_out(tableu, i, j):
-    """sweep out tableu table
+def sweep_out(tableau, i, j):
+    """sweep out tableau table
 
     Arguments:
-        tableu {ndarray} -- tableu table
+        tableau {ndarray} -- tableau table
         i {int} -- pivot index of column
         j {int} -- pivot index of row
 
     Returns:
-        tableu {ndarray} -- swept out tableu table
+        tableau {ndarray} -- swept out tableau table
     """
-    n_rows, n_cols = tableu.shape
+    n_rows, n_cols = tableau.shape
     for j_ in range(n_rows):
         if j_ != j:
-            alpha = tableu[j_][i] / tableu[j][i]
-            tableu[j_] -= alpha * tableu[j]
+            alpha = tableau[j_][i] / tableau[j][i]
+            tableau[j_] -= alpha * tableau[j]
 
-    return tableu
+    return tableau
 
-def reuse(A, b, c, tableu):
+def reuse(A, b, c, tableau):
 
     A = A.astype(np.float32)
 
@@ -119,63 +116,63 @@ def reuse(A, b, c, tableu):
         is_unit = True
         idx_one = None
         for j in range(m+1):
-            if abs(tableu[j][i] - 1) < eps:
+            if abs(tableau[j][i] - 1) < eps:
                 if idx_one is None:
                     idx_one = j #i列目で1が立っている行のindex
                 else:
                     break
-            elif (tableu[j][i]) >= eps:
+            elif (tableau[j][i]) >= eps:
                 break
 
         # i列目は単位行列
-        tableu[idx_one] *= -1
+        tableau[idx_one] *= -1
 
-    tableu = np.c_[tableu[:,:n+m].reshape(m+1,-1), tableu[:, -1].reshape(m+1, -1)]
-    tableu[-1] = np.c_[-c, np.zeros((1, m+1))]
+    tableau = np.c_[tableau[:,:n+m].reshape(m+1,-1), tableau[:, -1].reshape(m+1, -1)]
+    tableau[-1] = np.c_[-c, np.zeros((1, m+1))]
 
     for i in range(n):
         idx_nonzero = None
         n_zero = 0
         for j in range(m):
-            if tableu[j][i] < eps:
+            if tableau[j][i] < eps:
                 n_zero += 1
             else:
                 idx_nonzero = j
         if n_zero == m-1:
-            assert abs(tableu[idx_nonzero][i]) >= eps, "Error!"
+            assert abs(tableau[idx_nonzero][i]) >= eps, "Error!"
             assert idx_nonzero is not None, "Error!"
-            tableu = sweep_out(tableu, i, idx_nonzero)
-            tableu[idx_nonzero] /= tableu[idx_nonzero][i]
+            tableau = sweep_out(tableau, i, idx_nonzero)
+            tableau[idx_nonzero] /= tableau[idx_nonzero][i]
 
-    return tableu
+    return tableau
 
 
 def symplex():
     A = np.array([
-        [1,2,-3],
-        [-4,5,2],
-        [5,-3,-2]
+        [-1, 1],
+        [0, -1],
+        [1, 0]
     ])
-    b = np.array([0,-1,0])
-    c = np.array([[-2,2,1]])
-    tableu = make_tableu(A, b)
+    b = np.array([0,1,1])
+    c = np.array([[-1/2,1]])
+    tableau = make_tableau(A, b)
 
-    i, j = get_indices(tableu)
+    i, j = get_indices(tableau)
     while i is not None and j is not None:
-        tableu = sweep_out(tableu, i, j)
-        i, j = get_indices(tableu)
+        tableau = sweep_out(tableau, i, j)
+        i, j = get_indices(tableau)
 
-    print(pd.DataFrame(tableu))
-    tableu = reuse(A, b, c, tableu)
-    print(pd.DataFrame(tableu))
-    i, j = get_indices(tableu)
+    tableau = reuse(A, b, c, tableau)
+    i, j = get_indices(tableau)
     while i is not None and j is not None:
-        tableu = sweep_out(tableu, i, j)
-        print(i,j)
-        print(pd.DataFrame(tableu))
-        i, j = get_indices(tableu)
-        # print(pd.DataFrame(tableu))
-        
+        tableau = sweep_out(tableau, i, j)
+        print(pd.DataFrame(tableau))
+        i, j = get_indices(tableau)
+    assert j is not None, "LP has no solution (unbounded)"
+    print(pd.DataFrame(tableau))
+    return tableau[-1][-1]
+
 
 if __name__=="__main__":
-    symplex()
+    res = symplex()
+    print(res)
