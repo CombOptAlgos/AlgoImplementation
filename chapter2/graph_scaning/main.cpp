cf8df6d2da41d45b5e76e78f7b3b10d89981ae3b
Shion-0014 <seanmqe@gmail.com>
2019-10-12 22:03:10 +0900

    ADD: Graph Scanning Algo

---
+++ chapter2/graph_scaning/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+using Graph = vector<vector<int> >;
+
+Graph graph_scanning(Graph G, int N, int s) {
+
+    vector<bool> R(N, false);
+    vector<int> Q;
+    Graph T(N);
+    R[s] = true; //seen
+    Q.push_back(s); //todo
+
+    while (!Q.empty()) {
+
+        int v = Q[0]; // BFS
+
+        if (!G[v].empty()) {
+            rep(i, G[v].size()) {
+                int w = G[v][i];
+                if (!R[w]) // w in V/R
+                {
+                    R[w] = true;
+                    Q.push_back(w);
+                    T[v].push_back(w);
+                }
+            }
+        }
+        Q.erase(Q.begin()); // Q-v
+    }
+    return T;
+}
+
+
+int main(){
+
+    int N, M; cin >> N >> M;
+    int s; cin >> s;
+
+    Graph G(N);
+    rep(i, M){
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        // undirected graph
+        G[b].push_back(a);
+    }
+
+    Graph T = graph_scanning(G, N, s);
+
+    rep(i, N){
+        if (!T[i].empty()){
+            rep(j,T[i].size())
+            {
+                cout << i << T[i][j];
+                cout << endl;
+            }
+        }
+    }
+    return 0;
+}
