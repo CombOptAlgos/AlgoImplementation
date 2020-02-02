e2e0fff34d9684a2c5bafb0fcbb0560f9cdb1eb6
Shion-0014 <seanmqe@gmail.com>
2020-02-03 02:00:34 +0900

    MOD: main

--- chapter8/FORD_FULKERSON/main.cpp
+++ chapter8/FORD_FULKERSON/main.cpp
@@ -24,58 +24,75 @@ void add_edge_two_arrow(Graph &G, int from, int to, int capacity){
 }
 
 // f-augmenting pathの計算
-vector<int> dfs(Graph G, int v, int t, int N, bool &end_frag){
-    cout << "hello world!" << endl;
-    vector<int> path(N);
+vector<pair <int, int> > dfs(Graph G, int v, int t, int N, bool &end_frag){
+
+    vector< pair<int, int> > path(N);
     vector<bool> seen(N, false);
+    stack<int> S;
     end_frag = true;
 
     // 探索済みの頂点
     seen[v] = true;
+    S.push(v);
 
-    rep(i, G[v].size()){
-        if (G[v][i].capacity > 0){
-            int next_v = G[v][i].to;
+    while(!S.empty()){
+       v = S.top();
+       S.pop();
 
-            path[v] = i; // next_v = G[v][path[v]].to
-            if (next_v == t){
-                end_frag = false;
-                break;
-            }
+       rep(i, G[v].size()){
+           if (G[v][i].capacity > 0){
+               int next_v = G[v][i].to;
 
-            if (!seen[next_v]) dfs(G, next_v, t, N, end_frag);
-        }
+               if (!seen[next_v]){
+                   seen[next_v] = true;
+                   path[next_v] = make_pair(v, i); // 一度seenがtrueになるともうアクセスされないので、これはユニークに定まる
+                   S.push(next_v);
+               }
+
+               // tに届くと終了する
+               if (next_v == t){
+                   end_frag = false;
+                   break;
+               }
+           }
+       }
+       if (!end_frag) break;
     }
     return path;
 }
 
 // compute gamma
-int compute_gamma(Graph G, vector<int> path, int v, int t){
+int compute_gamma(Graph G, vector<pair<int, int> > path, int s, int v){
 
     int gamma = INF;
 
     while(1){
-        gamma = (gamma < G[v][path[v]].capacity ? gamma : G[v][path[v]].capacity);
-        v = G[v][path[v]].to;
-        if (v == t) break;
+        int index = path[v].second;
+        v = path[v].first;
+
+        gamma = (gamma < G[v][index].capacity ? gamma : G[v][index].capacity);
+
+        if (v == s) break;
     }
 
     return gamma;
 }
 
 // augment
-void augment(Graph &G, vector<int> path, int gamma, int v, int t){
+void augment(Graph &G, vector<pair<int, int> > path, int gamma, int s, int v){
 
     while(1){
 
+        int index = path[v].second;
+        v = path[v].first;
+
         // path上の辺
-        edge e = G[v][path[v]];
-        e.capacity = e.capacity - gamma;
+        G[v][index].capacity = G[v][index].capacity - gamma;
         // path上の辺の逆辺
+        edge e = G[v][index];
         G[e.to][e.rev_id].capacity = G[e.to][e.rev_id].capacity + gamma;
 
-        v = G[v][path[v]].to;
-        if (v == t) break;
+        if (v == s) break;
     }
 }
 
@@ -98,17 +115,14 @@ int main(){
     // FORD FULKERSON
     while(1){
         // find augmenting s-t path
-        vector<int> path = dfs(G2, s, t, N, end_frag);
-        cout << "dfs ok" << endl;
+        vector<pair<int, int> > path = dfs(G2, s, t, N, end_frag);
         if (end_frag) break;
 
         // compute gamma
         int gamma = compute_gamma(G2, path, s, t);
-        cout << "gamma" << endl;
 
         // augment f along path by gamma
         augment(G2, path, gamma, s, t);
-        cout << "augment" << endl;
     }
 
     // max flowの出力
